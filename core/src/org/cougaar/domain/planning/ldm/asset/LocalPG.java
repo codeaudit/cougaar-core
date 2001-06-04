73f59bd0c0c572b7fc0dc93edc29ced5a12606dd
ngivler <ngivler>
2001-06-04 13:00:52 +0000

    Added support for implementing relationship schedules with a PropertyGroup. RelationshipPG (backed by RelationshipBG) is defined in properties.def.
    Added LocalPG - a marker for PropertyGroups whose contents are local to the
    cluster. Currently used for RelationshipPG and LocationSchedulePG.

---
+++ core/src/org/cougaar/domain/planning/ldm/asset/LocalPG.java
@@ -0,0 +1,19 @@
+/*
+ * <copyright>
+ * Copyright 1997-2001 Defense Advanced Research Projects
+ * Agency (DARPA) and ALPINE (a BBN Technologies (BBN) and
+ * Raytheon Systems Company (RSC) Consortium).
+ * This software to be used only in accordance with the
+ * COUGAAR licence agreement.
+ * </copyright>
+ */
+
+
+package org.cougaar.domain.planning.ldm.asset;
+
+/** marker class for indicating that a PG contains information which is 
+ * to the current cluster.
+ **/
+public interface LocalPG {
+}
+
---
+++ core/src/org/cougaar/domain/planning/ldm/asset/RelationshipBG.java
@@ -0,0 +1,98 @@
+/*--------------------------------------------------------------------------
+ *                         RESTRICTED RIGHTS LEGEND
+ *
+ *   Use, duplication, or disclosure by the Government is subject to
+ *   restrictions as set forth in the Rights in Technical Data and Computer
+ *   Software Clause at DFARS 52.227-7013.
+ *
+ *                             BBNT Solutions LLC,
+ *                             10 Moulton Street
+ *                            Cambridge, MA 02138
+ *                              (617) 873-3000
+ *
+ *   Copyright 2000 by
+ *             BBNT Solutions LLC,
+ *             all rights reserved.
+ *
+ * --------------------------------------------------------------------------*/
+package org.cougaar.domain.planning.ldm.asset;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.cougaar.domain.planning.ldm.plan.HasRelationships;
+import org.cougaar.domain.planning.ldm.plan.RelationshipScheduleImpl;
+
+public class RelationshipBG implements PGDelegate {
+  protected transient NewRelationshipPG myPG;
+  
+  public RelationshipBG(NewRelationshipPG pg, 
+                        HasRelationships hasRelationships) {
+    myPG = (NewRelationshipPG) pg;
+
+    RelationshipScheduleImpl pgSchedule = (RelationshipScheduleImpl) pg.getRelationshipSchedule();
+    if ((pgSchedule == null) ||
+        (pgSchedule.isEmpty())){
+      init(hasRelationships);
+    } else if (!pgSchedule.getHasRelationships().equals(hasRelationships)) {
+      throw new java.lang.IllegalArgumentException("");
+    }
+
+  }
+
+  
+  public PGDelegate copy(PropertyGroup pg) { 
+    if (!(pg instanceof NewRelationshipPG)) {
+      throw new java.lang.IllegalArgumentException("Property group must be a RelationshipPG");
+    }
+
+    NewRelationshipPG relationshipPG = (NewRelationshipPG ) pg;
+    return new RelationshipBG(relationshipPG, 
+                              relationshipPG.getRelationshipSchedule().getHasRelationships());
+  }
+
+  public void readObject(ObjectInputStream in) {
+    try {
+     in.defaultReadObject();
+
+     if (in instanceof org.cougaar.core.cluster.persist.PersistenceInputStream){
+       myPG = (NewRelationshipPG) in.readObject();
+     } else {
+       // If not persistence, need to initialize the relationship schedule
+       myPG = (NewRelationshipPG) in.readObject();
+       init(myPG.getRelationshipSchedule().getHasRelationships());
+     }
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+      throw new RuntimeException();
+    } catch (ClassNotFoundException cnfe) {
+      cnfe.printStackTrace();
+      throw new RuntimeException();
+    }
+  }       
+
+  public void writeObject(ObjectOutputStream out) {
+    try {
+      // Make sure that it agrees with schedule
+      out.defaultWriteObject();
+      
+      if (out instanceof org.cougaar.core.cluster.persist.PersistenceOutputStream) {
+        out.writeObject(myPG);
+      } else {
+        // Clear schedule before writing out
+        myPG.getRelationshipSchedule().clear();
+        out.writeObject(myPG);
+      }
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+      throw new RuntimeException();
+    }
+  }
+
+  public void init(HasRelationships hasRelationships) {
+    myPG.setRelationshipSchedule(new RelationshipScheduleImpl(hasRelationships));
+  }
+}
+
+
