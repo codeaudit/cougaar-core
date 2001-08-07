01099d3174f1b9089f84a4e2c125bc3d3c6cb646
tomlinso <tomlinso>
2001-08-07 11:25:33 +0000

    I thought I deleted these. Naming service files are in o/c/naming.

--- core/src/org/cougaar/core/nameservice/NameService.java
+++
@@ -1,9 +0,0 @@
-package org.cougaar.core.nameservice;
-
-import javax.naming.directory.InitialDirContext;
-import javax.naming.NamingException;
-import org.cougaar.core.component.Service;
-
-public interface NameService extends Service {
-    InitialDirContext getRootContext() throws NamingException;
-}
