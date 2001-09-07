2606d1ce4431f292ab2ea9d6d42f21b31dd2346a
mdavis <mdavis>
2001-09-07 20:34:13 +0000

    Interface for returns from getOutputType on the Logging Control Service.

---
+++ core/src/org/cougaar/core/cluster/LoggingOutputType.java
@@ -0,0 +1,33 @@
+/*
+ * <copyright>
+ * Copyright 1997-2001 Defense Advanced Research Projects
+ * Agency (DARPA) and ALPINE (a BBN Technologies (BBN) and
+ * Raytheon Systems Company (RSC) Consortium).
+ * This software to be used only in accordance with the
+ * COUGAAR licence agreement.
+ * </copyright>
+ */
+
+package org.cougaar.core.cluster;
+
+import org.cougaar.core.component.*;
+import java.util.Enumeration;
+
+/** a Service for controlling logging
+ **/
+
+public interface LoggingOutputType {
+
+  public static final int CONSOLE = LoggingControlService.CONSOLE;
+  public static final int STREAM  = LoggingControlService.STREAM;
+  public static final int FILE    = LoggingControlService.FILE;
+
+  public String getNode();
+  public int getOutputType();
+  public String getOutputDevice();
+
+}
+
+
+
+
