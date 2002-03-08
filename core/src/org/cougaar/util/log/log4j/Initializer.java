997fbc5a4f6f7fa1ee4a9c321cdb7ff3adee1d83
twright <twright>
2002-03-08 22:36:40 +0000

    Set the default logging level to "WARN", and the standard-out like "2002-03-08 22:26:34,898 WARN [YourPlugin] - your message".

--- core/src/org/cougaar/util/log/log4j/Initializer.java
+++ core/src/org/cougaar/util/log/log4j/Initializer.java
@@ -21,6 +21,34 @@ import org.apache.log4j.PropertyConfigurator;
  */
 class Initializer {
 
+  /**
+   * Default configuration only prints WARN or higher
+   * statements.
+   * <p>
+   * A client with name "x.y.z" that calls:<pre>
+   *   log.warn("test message");
+   * </pre>
+   * will generate a standard-output message similar to:</pre>
+   *   2002-03-08 22:26:08,980 WARN [z] - test message
+   * </pre>.
+   * <p>
+   * See the log4j docs for further details.
+   */
+  private static final String[][] DEFAULT_PROPS = {
+    {"log4j.rootCategory", 
+      "WARN,A1"
+    },
+    {"log4j.appender.A1", 
+      "org.apache.log4j.ConsoleAppender"
+    },
+    {"log4j.appender.A1.layout", 
+      "org.apache.log4j.PatternLayout"
+    },
+    {"log4j.appender.A1.layout.ConversionPattern", 
+      "%d{ISO8601} %-5p [%c{1}] - %m%n"
+    },
+  };
+
   public static void configure(Properties props) {
     // flatten props to map
     if (props != null) {
@@ -45,14 +73,36 @@ class Initializer {
       configure((Properties) m);
     } else {
       int n = ((m != null) ? m.size() : 0);
+      // log4j uses properties
+      Properties props;
       if (n > 0) {
-        // log4j uses properties
-        Properties props = new Properties();
+        props = new Properties();
         props.putAll(m);
-        PropertyConfigurator.configure(props);
       } else {
-        BasicConfigurator.configure();
+        props = getDefaultProperties();
       }
+      PropertyConfigurator.configure(props);
     }
   }
+
+  private static Properties DEFAULT_PROPERTIES;
+
+  /**
+   * convert "String[][] DEFAULT_PROPS" to Properties.
+   */
+  private static synchronized Properties getDefaultProperties() {
+    if (DEFAULT_PROPERTIES == null) {
+      Properties p = new Properties();
+      try {
+        for (int i = 0, n = DEFAULT_PROPS.length; i < n; i++) {
+          p.put(DEFAULT_PROPS[i][0], DEFAULT_PROPS[i][1]);
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      DEFAULT_PROPERTIES = p;
+    }
+    return DEFAULT_PROPERTIES;
+  }
+
 }
