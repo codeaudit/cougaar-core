ff385200265ba77501e0fd6a86615708edf86051
tomlinso <tomlinso>
2002-04-01 13:11:47 +0000

    Rename "SensorMeasurement" to the more generic "Condition"

---
+++ core/src/org/cougaar/core/service/ConditionService.java
@@ -0,0 +1,51 @@
+package org.cougaar.core.service;
+
+import org.cougaar.core.component.Service;
+import org.cougaar.core.persist.NotPersistable;
+import org.cougaar.core.adaptivity.Condition;
+import java.util.Set;
+
+/** 
+ * This service looks up sensor data in the blackboard, 
+ * For use by the AdaptivityEngine
+ */
+public interface ConditionService extends Service {
+  /**
+   * The interface to be implemented by listener objects for this
+   * service. Note that no implementation of the methods of this
+   * interface should do any more than set variables within the
+   * object. In particular, they should not use any services or
+   * synchronize on or wait for anything.
+   *
+   * There are currently no methods defined
+   **/
+  interface Listener extends NotPersistable {
+  }
+
+  /**
+   * Get a Condition object by name.
+   **/ 
+  Condition getConditionByName(String sensor);
+
+  /**
+   * Get the names of all known Conditions.
+   **/
+  Set getAllConditionNames();
+
+  /**
+   * Add a listener object. The given object will be publishChanged
+   * whenever any Condition is added, removed, or changed. The
+   * Object must already have been publishedAdded to the blackboard by
+   * the caller.
+   **/
+  void addListener(Listener cb);
+  /**
+   * Remove a listener object. The given object will no longer be
+   * publishChanged whenever any Condition is added, removed,
+   * or changed. The Object should not be removed from the blackboard
+   * until it has been removed as a listener.
+   **/
+  void removeListener(Listener cb);
+}
+
+
