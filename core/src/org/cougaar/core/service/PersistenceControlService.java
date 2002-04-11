24d587fb829b96a3722aa9ccfc2eacb5b7126a64
tomlinso <tomlinso>
2002-04-11 16:37:39 +0000

    Checkin the real version of this service interface. Thanks, Noelle.

--- core/src/org/cougaar/core/service/PersistenceControlService.java
+++ core/src/org/cougaar/core/service/PersistenceControlService.java
@@ -1,6 +1,6 @@
 /*
  * <copyright>
- *  Copyright 1997-2001 BBNT Solutions, LLC
+ *  Copyright 2001 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
  * 
  *  This program is free software; you can redistribute it and/or modify
@@ -18,35 +18,70 @@
  *  PERFORMANCE OF THE COUGAAR SOFTWARE.
  * </copyright>
  */
-
 package org.cougaar.core.service;
 
-import org.cougaar.core.adaptivity.OMCRangeList;
 import org.cougaar.core.component.Service;
-
-/** Major hack to get the build to compile. Reverse engineered from 
- *  BasePersistence.java
- */
+import org.cougaar.core.adaptivity.OMCRangeList;
 
 public interface PersistenceControlService extends Service {
-  public String[] getControlNames();
+    /**
+     * Gets the names of all controls (operating modes). These are
+     * controls over persistence as a whole, not media-specific.
+     * @return an array of all control names
+     **/
+    String[] getControlNames();
 
-  public OMCRangeList getControlValues(String controlName);
-  
-  public void setControlValue(String controlName, Comparable newValue);
-  
-  public String[] getMediaNames();
-  
-  public String[] getMediaControlNames(String mediaName);
-  
-  public OMCRangeList getMediaControlValues(String mediaName, String controlName);
-  
-  public void setMediaControlValue(String mediaName,
-                                   String controlName,
-                                   Comparable newValue);
-}  
+    /**
+     * Gets the (allowed) values of a given control. Values used to
+     * set a control must be in the ranges specified by the return.
+     * @return a list of ranges of values that are allowed for the
+     * named control.
+     * @param controlName the name of a persistence-wide control
+     **/
+    OMCRangeList getControlValues(String controlName);
 
+    /**
+     * Sets the value for the named control. The value must be in the
+     * list or ranges returned by {@link #getControlValues}.
+     * @param controlName the name of the control to set.
+     * @param newValue the new value for the control. Must be in the
+     * allowed ranges.
+     **/
+    void setControlValue(String controlName, Comparable newValue);
 
+    /**
+     * Gets the names of the installed media plugins.
+     * @return an array of the names of the installed media plugins.
+     **/
+    String[] getMediaNames();
 
+    /**
+     * Gets the names of the controls for the named media (plugin).
+     * @return an array of all control names for the given media.
+     * @param mediaName the name of the media.
+     **/
+    String[] getMediaControlNames(String mediaName);
 
+    /**
+     * Gets the allowed values of then named control for the named
+     * media plugin. Values used to set a control must be in the
+     * ranges specified by the return.
+     * @return a list of ranges of values that are allowed for the
+     * named control.
+     * @param mediaName the name of the media having the control
+     * @param controlName the name of a media-specific control
+     * @return a list of the allowed value ranges.
+     **/
+    OMCRangeList getMediaControlValues(String mediaName, String controlName);
 
+    /**
+     * Sets the value for the named control for the named media
+     * plugin. The value must be in the list or ranges returned by
+     * {@link #getMediaControlValues}.
+     * @param mediaName the name of the media having the control
+     * @param controlName the name of the control to set.
+     * @param newValue the new value for the control. Must be in the
+     * allowed ranges.
+     **/
+    void setMediaControlValue(String mediaName, String controlName, Comparable newValue);
+}
