2f9e134bf14971cd2cedb1b91aecc2232d9e9ae2
tomlinso <tomlinso>
2002-04-19 19:37:43 +0000

    * org/cougaar/core/relay/Relay.java: Move getSource method out of the Relay base interface. It is only needed in the Relay.Target interface. Define bit flags constants for marking return values from updateContent and updateResponse methods.

--- core/src/org/cougaar/core/relay/Relay.java
+++ core/src/org/cougaar/core/relay/Relay.java
@@ -38,19 +38,13 @@ import org.cougaar.core.util.UniqueObject;
  * multiple-targets or target-responses.
  */
 public interface Relay extends UniqueObject {
+  // The kinds of changes caused by updateResponse and updateContent
+  public static final int NO_CHANGE       = 0;
+  public static final int CONTENT_CHANGE  = 1;
+  public static final int RESPONSE_CHANGE = 2;
 
   // UID support from UniqueObject -- note that the
   // Source and Target(s) all have the same UID.
-
-  /**
-   * Get the address of the Agent holding the Source 
-   * copy of this Relay, or null if it is the local agent.
-   * <p>
-   * If the source is null or matches the local agent, then 
-   * the Relay is cast to a "Relay.Source", otherwise it is 
-   * cast to a "Relay.Target".
-   **/
-  MessageAddress getSource();
 
   /**
    * The source-side Relay, which contains the content and
@@ -67,7 +61,16 @@ public interface Relay extends UniqueObject {
 
     /**
      * Get an object representing the value of this Relay
-     * suitable for transmission.
+     * suitable for transmission. Caution!!! The content should, in
+     * general <em>not</em> contain targets, source, responses,
+     * factories or anything else that is not essential for specifying
+     * the content. For implementations wishing to use the same class
+     * for implementing Relay.Source, Relay.Target, and Relay.Content,
+     * these non-essential items should be declared transient and
+     * writeObject and readObject methods written to serialize these
+     * items only for persistence, that is, only if the object streams
+     * are instances of PersistenceOutputStream or
+     * PersistenceInputStream.
      */
     Object getContent();
 
@@ -80,12 +83,15 @@ public interface Relay extends UniqueObject {
 
     /**
      * Update the source with the new response.
-     * @return true if the update changed the Relay, in which
-     *    case the infrastructure should "publishChange" this 
-     *    Relay.
+     * @return NO_CHANGE if the update caused no change to the source<br>
+     *         RESPONSE_CHANGED if the source is also a target and the
+     * incoming response requires that a new outgoing response be sent
+     * to the source.<br>
+     *         CONTENT_CHANGED if the update has changed the content of
+     * the source<br>
+     *         RESPONSE_CHANGED|CONTENT_CHANGED if both changes occurred
      */
-    boolean updateResponse(MessageAddress target, Object response);
-
+    int updateResponse(MessageAddress target, Object response);
   }
 
   /**
@@ -95,6 +101,13 @@ public interface Relay extends UniqueObject {
   interface Target extends Relay {
 
     /**
+     * Get the address of the Agent holding the Source copy of this
+     * Relay. If null is returned, then responses will not be sent.
+     * @return the address from which this Relay came.
+     **/
+    MessageAddress getSource();
+
+    /**
      * Get the current Response for this target. Null indicates that
      * this target has no response.
      */
@@ -102,11 +115,14 @@ public interface Relay extends UniqueObject {
 
     /**
      * Update the target with the new content.
-     * @return true if the update changed the Relay, in which
-     *    case the infrastructure should "publishChange" this 
+     * @return NO_CHANGE if the update caused no change to the target<br>
+     *         RESPONSE_CHANGED if the update has changed the response to
+     * be sent to the source.<br>
+     *         CONTENT_CHANGED if the update has changed the content of
+     * the target<br>
+     *         RESPONSE_CHANGED|CONTENT_CHANGED if both changes occurred
      */
-    boolean updateContent(Object content, Token token);
-
+    int updateContent(Object content, Token token);
   }
 
   /**
