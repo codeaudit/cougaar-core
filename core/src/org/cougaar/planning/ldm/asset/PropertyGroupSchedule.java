2386c7862747e16d8ca30e91fb5ca94ae411b1ee
ngivler <ngivler>
2002-05-08 14:18:54 +0000

    Remove support of LockedPGSchedule while I test a work around for Jikes bug.

--- core/src/org/cougaar/planning/ldm/asset/PropertyGroupSchedule.java
+++ core/src/org/cougaar/planning/ldm/asset/PropertyGroupSchedule.java
@@ -28,9 +28,7 @@ import java.beans.MethodDescriptor;
 import java.beans.SimpleBeanInfo;
 import java.beans.PropertyDescriptor;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.Iterator;
 
 import org.cougaar.util.NewTimeSpan;
@@ -164,6 +162,9 @@ public class PropertyGroupSchedule extends NonOverlappingTimeSpanSet
       myTiling = fill((NewTimeSpan) myDefault.copy());
     }
 
+    Exception e = new RuntimeException("Tiling count: " + myTiling.size());
+    e.printStackTrace();
+
     return new NonOverlappingTimeSpanSet(myTiling.intersectingSet(startTime, 
                                                                   endTime));
   }
@@ -186,29 +187,12 @@ public class PropertyGroupSchedule extends NonOverlappingTimeSpanSet
     RuntimeException e = new RuntimeException();
     e.printStackTrace();
     */
-    // Shallow clone
     PropertyGroupSchedule schedule = new PropertyGroupSchedule(this);
     schedule.lockPGs();
 
     return schedule;
   }
 
-  private transient PropertyGroupSchedule _locked = null;
-  public PropertyGroupSchedule lock(Object key) {
-    if (_locked == null) {
-      _locked = new _Locked(key);
-    }
-    return _locked;
-  }
-
-  public PropertyGroupSchedule lock() {
-    return lock(null);
-  }
-
-  public PropertyGroupSchedule unlock(Object key) { 
-    return this;
-  }
-   
   public void lockPGs(Object key) {
     if (getDefault() != null) {
       setDefault((TimePhasedPropertyGroup)getDefault().lock(key));
@@ -249,85 +233,52 @@ public class PropertyGroupSchedule extends NonOverlappingTimeSpanSet
     return tppgClass.equals(myPropertyGroupClass);
   }
 
-  public static void main(String arg[]) {
-    
-    PropertyGroupSchedule schedule = new PropertyGroupSchedule();
+//    public static void main(String arg[]) {
 
-    CommunityPGImpl defaultPG = new CommunityPGImpl();
-    ArrayList defaultCommunities = new ArrayList();
-    defaultCommunities.add("default");
-    defaultPG.setTimeSpan(TimeSpan.MIN_VALUE, TimeSpan.MAX_VALUE);
-    defaultPG.setCommunities(defaultCommunities);
-    
-    CommunityPGImpl item1 = new CommunityPGImpl();
-    ArrayList firstCommunities = new ArrayList();
-    firstCommunities.add("Item1");
-    item1.setTimeSpan(-5, 20);
-    item1.setCommunities(firstCommunities);
-    
-    CommunityPGImpl item2 = new CommunityPGImpl();
-    ArrayList secondCommunities = new ArrayList();
-    secondCommunities.add("Item2");
-    item2.setTimeSpan(30, 90);
-    item2.setCommunities(secondCommunities);
-    
-    schedule.setDefault(defaultPG);
-    schedule.add(item1);
-    schedule.add(item2);
-    
-    System.out.println("Initial");
-    Iterator iterator = schedule.iterator();
-    while(iterator.hasNext()) {
-      CommunityPG pg = (CommunityPG)iterator.next();
-      System.out.println("\t" + pg.getCommunities() + " " + 
-                         pg.getStartTime() + " " + 
-                         pg.getEndTime());
-    }
-    
-    System.out.println("Filled");
-    iterator = 
-      schedule.getTiling(TimeSpan.MIN_VALUE, TimeSpan.MAX_VALUE).iterator();
-    while(iterator.hasNext()) {
-      CommunityPG pg = (CommunityPG)iterator.next();
-      System.out.println("\t" + pg.getCommunities() + " " + 
-                         pg.getStartTime() + " " + 
-                         pg.getEndTime());
-    }
- 
-    PropertyGroupSchedule locked = schedule.lock();
-    System.out.println("locked size: " + locked.size());
+//      PropertyGroupSchedule schedule = new PropertyGroupSchedule();
 
-    System.out.println("schedule == locked: " + locked.equals(schedule));
+//      ItemIdentificationPGImpl defaultPG = new ItemIdentificationPGImpl();
+//      defaultPG.setTimeSpan(TimeSpan.MIN_VALUE, TimeSpan.MAX_VALUE);
+//      defaultPG.setItemIdentification("DEFAULT");
+//      defaultPG.setNomenclature("DEFAULT");
+//      defaultPG.setAlternateItemIdentification("DEFAULT");
 
-    System.out.println("Locked");
-    iterator = locked.iterator();
-    while(iterator.hasNext()) {
-      CommunityPG pg = (CommunityPG)iterator.next();
-      System.out.println("\t" + pg.getCommunities() + " " + 
-                         pg.getStartTime() + " " + 
-                         pg.getEndTime());
-    }
+//      ItemIdentificationPGImpl item1 = new ItemIdentificationPGImpl();
+//      item1.setTimeSpan(-5, 20);
+//      item1.setItemIdentification("ITEM1");
+//      item1.setNomenclature("ITEM1");
+//      item1.setAlternateItemIdentification("ITEM1");
+
+//      ItemIdentificationPGImpl item2 = new ItemIdentificationPGImpl();
+//      item2.setTimeSpan(30, 90);
+//      item2.setItemIdentification("ITEM2");
+//      item2.setNomenclature("ITEM2");
+//      item2.setAlternateItemIdentification("ITEM2");
+
+//      schedule.setDefault(defaultPG);
+//      schedule.add(item1);
+//      schedule.add(item2);
+
+//      System.out.println("Initial");
+//      Iterator iterator = schedule.iterator();
+//      while(iterator.hasNext()) {
+//        ItemIdentificationPG pg = (ItemIdentificationPG)iterator.next();
+//        System.out.println("\t" + pg.getItemIdentification() + " " + 
+//                           pg.getStartTime() + " " + 
+//                           pg.getEndTime());
+//      }
     
-    System.out.println("Locked Filled");
-    iterator = 
-      locked.getTiling(TimeSpan.MIN_VALUE, TimeSpan.MAX_VALUE).iterator();
-    while(iterator.hasNext()) {
-      CommunityPG pg = (CommunityPG)iterator.next();
-      System.out.println("\t" + pg.getCommunities() + " " + 
-                         pg.getStartTime() + " " + 
-                         pg.getEndTime());
-    }
+//      System.out.println("Filled");
+//      iterator = 
+//        schedule.getTiling(TimeSpan.MIN_VALUE, TimeSpan.MAX_VALUE).iterator();
+//      while(iterator.hasNext()) {
+//        ItemIdentificationPG pg = (ItemIdentificationPG)iterator.next();
+//        System.out.println("\t" + pg.getItemIdentification() + " " + 
+//                           pg.getStartTime() + " " + 
+//                           pg.getEndTime());
+//      }
 
-    System.out.println("locked: pgClass " + locked.getPGClass());
-    System.out.println("schedule: pgClass " + schedule.getPGClass());
-
-    try {
-      locked.setDefault(defaultPG);
-      System.out.println("Error: able to setDefault on locked");
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+//    }
 
   /** @return the method name on an asset to retrieve the PG **/
   public String getAssetGetMethod() {
@@ -372,290 +323,102 @@ public class PropertyGroupSchedule extends NonOverlappingTimeSpanSet
     return PropertyGroupSchedule.class;
   }
 
-  /**
-   * Deny knowledge about the class and customizer of the bean.
-   * You can override this if you wish to provide explicit info.
-   */
-  public BeanDescriptor getBeanDescriptor() {
-    return null;
-  }
-  
-  /**
-   * Deny knowledge of a default property. You can override this
-   * if you wish to define a default property for the bean.
-   */
-  public int getDefaultPropertyIndex() {
-    return -1;
-  }
-  
-  /**
-   * Deny knowledge of event sets. You can override this
-   * if you wish to provide explicit event set info.
-   */
-  public EventSetDescriptor[] getEventSetDescriptors() {
-    return null;
-  }
-  
-  /**
-   * Deny knowledge of a default event. You can override this
-   * if you wish to define a default event for the bean.
-   */
-  public int getDefaultEventIndex() {
-    return -1;
-  }
-  
-  /**
-   * Deny knowledge of methods. You can override this
-   * if you wish to provide explicit method info.
-   */
-  public MethodDescriptor[] getMethodDescriptors() {
-    return null;
-  }
-  
-  /**
-   * Claim there are no other relevant BeanInfo objects.  You
-   * may override this if you want to (for example) return a
-   * BeanInfo for a base class.
-   */
-  public BeanInfo[] getAdditionalBeanInfo() {
-    return null;
-  }
-  
-  /**
-   * Claim there are no icons available.  You can override
-   * this if you want to provide icons for your bean.
-   */
-  public java.awt.Image getIcon(int iconKind) {
-    return null;
-  }
-  
-  /**
-   * This is a utility method to help in loading icon images.
-   * It takes the name of a resource file associated with the
-   * current object's class file and loads an image object
-   * from that file.  Typically images will be GIFs.
-   * <p>
-   * @param resourceName  A pathname relative to the directory
-   *		holding the class file of the current class.  For example,
-   *		"wombat.gif".
-   * @return  an image object.  May be null if the load failed.
-   */
-  public java.awt.Image loadImage(final String resourceName) {
-    try {
-      final Class c = getClass();
-      java.awt.image.ImageProducer ip = (java.awt.image.ImageProducer)
-        java.security.AccessController.doPrivileged(
-                                                    new java.security.PrivilegedAction() {
-                                                      public Object run() {
-                                                        java.net.URL url;
-                                                        if ((url = c.getResource(resourceName)) == null) {
-                                                          return null;
-                                                        } else {
-                                                          try {
-                                                            return url.getContent();
-                                                          } catch (java.io.IOException ioe) {
-                                                            return null;
-                                                          }
-                                                        }
-                                                      }
-                                                    });
-      
-      if (ip == null)
-        return null;
-      java.awt.Toolkit tk = java.awt.Toolkit.getDefaultToolkit();
-      return tk.createImage(ip);
-    } catch (Exception ex) {
-      return null;
-    }
-  }
-  
-  private final class _Locked extends PropertyGroupSchedule 
-    implements LockedPGSchedule {
-
-    private transient Object theKey = null;
-    _Locked(Object key) { 
-      if (this.theKey == null){  
-        this.theKey = key; 
-      } 
-    }  
-
-    /** public constructor for beaninfo - probably wont work**/
-    public _Locked() {}
-
-    public PropertyGroupSchedule lock() { return this; }
-    public PropertyGroupSchedule lock(Object o) { return this; }
-
-    public PropertyGroupSchedule unlock(Object key) {
-       if( theKey.equals(key) )
-         return PropertyGroupSchedule.this;
-       else 
-         throw new IllegalArgumentException("unlock: mismatched internal and provided keys!");
+    /**
+     * Deny knowledge about the class and customizer of the bean.
+     * You can override this if you wish to provide explicit info.
+     */
+    public BeanDescriptor getBeanDescriptor() {
+	return null;
     }
 
-    public PropertyGroupSchedule copy() {
-      return (PropertyGroupSchedule) clone();
+    /**
+     * Deny knowledge of a default property. You can override this
+     * if you wish to define a default property for the bean.
+     */
+    public int getDefaultPropertyIndex() {
+	return -1;
     }
 
-
-    public Object clone() {
-      return new PropertyGroupSchedule(PropertyGroupSchedule.this);
+    /**
+     * Deny knowledge of event sets. You can override this
+     * if you wish to provide explicit event set info.
+     */
+    public EventSetDescriptor[] getEventSetDescriptors() {
+	return null;
     }
 
-    public boolean equals(Object o) {
-      return PropertyGroupSchedule.this.equals(o);
+    /**
+     * Deny knowledge of a default event. You can override this
+     * if you wish to define a default event for the bean.
+     */
+    public int getDefaultEventIndex() {
+	return -1;
     }
 
-    public int hashCode() {
-      return PropertyGroupSchedule.this.hashCode();
+    /**
+     * Deny knowledge of methods. You can override this
+     * if you wish to provide explicit method info.
+     */
+    public MethodDescriptor[] getMethodDescriptors() {
+	return null;
     }
 
-
-    public void initializePGClass(Class tppgClass) {
-      throw new UnsupportedOperationException();
+    /**
+     * Claim there are no other relevant BeanInfo objects.  You
+     * may override this if you want to (for example) return a
+     * BeanInfo for a base class.
+     */
+    public BeanInfo[] getAdditionalBeanInfo() {
+	return null;
     }
 
-    public void initializePGClass(PropertyGroup propertyGroup) {
-      throw new UnsupportedOperationException();
-    }
-                                                
-    public Class getPGClass() {
-      return PropertyGroupSchedule.this.getPGClass();
-    }
-
-    public void setDefault(TimePhasedPropertyGroup defaultPG) {
-      throw new UnsupportedOperationException();
+    /**
+     * Claim there are no icons available.  You can override
+     * this if you want to provide icons for your bean.
+     */
+    public java.awt.Image getIcon(int iconKind) {
+	return null;
     }
 
-    public TimePhasedPropertyGroup getDefault() {
-      return PropertyGroupSchedule.this.getDefault();
-    }
+    /**
+     * This is a utility method to help in loading icon images.
+     * It takes the name of a resource file associated with the
+     * current object's class file and loads an image object
+     * from that file.  Typically images will be GIFs.
+     * <p>
+     * @param resourceName  A pathname relative to the directory
+     *		holding the class file of the current class.  For example,
+     *		"wombat.gif".
+     * @return  an image object.  May be null if the load failed.
+     */
+    public java.awt.Image loadImage(final String resourceName) {
+	try {
+	    final Class c = getClass();
+	    java.awt.image.ImageProducer ip = (java.awt.image.ImageProducer)
+		java.security.AccessController.doPrivileged(
+		new java.security.PrivilegedAction() {
+		    public Object run() {
+			java.net.URL url;
+			if ((url = c.getResource(resourceName)) == null) {
+			    return null;
+			} else {
+			    try {
+				return url.getContent();
+			    } catch (java.io.IOException ioe) {
+				return null;
+			    }
+			}
+		    }
+	    });
 
-    public void clearDefault() {
-      throw new UnsupportedOperationException();
+	    if (ip == null)
+		return null;
+	    java.awt.Toolkit tk = java.awt.Toolkit.getDefaultToolkit();
+	    return tk.createImage(ip);
+	} catch (Exception ex) {
+	    return null;
+	}
     }
-
-    public boolean add(Object o) {
-      throw new UnsupportedOperationException();
-    }
-
-    public boolean remove(Object o) {
-      throw new UnsupportedOperationException();
-    }
-
-    public Object remove(int index) {
-      throw new UnsupportedOperationException();
-    }
-
-    public boolean removeAll(Collection c) {
-      throw new UnsupportedOperationException();
-    }
-
-    public boolean retainAll(Collection c) {
-      throw new UnsupportedOperationException();
-    }
-    
-      
-    public void clear() {
-      throw new UnsupportedOperationException();
-    }
-
-    public Iterator iterator() {
-      return PropertyGroupSchedule.this.iterator();
-    }
-
-    public NonOverlappingTimeSpanSet getTiling(long startTime, long endTime) {
-      return PropertyGroupSchedule.this.getTiling(startTime, endTime);
-    }
-
-    public NonOverlappingTimeSpanSet getTiling(TimeSpan timeSpan) {
-      return PropertyGroupSchedule.this.getTiling(timeSpan);
-    }
-      
-
-    public NonOverlappingTimeSpanSet getTiling() {
-      return PropertyGroupSchedule.this.getTiling();
-    }
-
-    public Object[] getTilingAsArray() {
-      return PropertyGroupSchedule.this.getTilingAsArray();
-    }
-
-    public TimeSpan intersects(final long time) {
-      return PropertyGroupSchedule.this.intersects(time);
-    }
-
-    public NonOverlappingTimeSpanSet fill(NewTimeSpan filler) {
-      return PropertyGroupSchedule.this.fill(filler);
-    }
-
-    public void add(int i, Object o) {
-      throw new UnsupportedOperationException();
-    }
-
-    public boolean addAll(Collection c) {
-      throw new UnsupportedOperationException();
-    }
-
-    public boolean addAll(int index, Collection c) {
-    throw new UnsupportedOperationException();
-    }
-
-    public boolean contains(Object o) {
-      return PropertyGroupSchedule.this.contains(o);
-    }
-
-    public Object set(int index, Object element) {
-      throw new UnsupportedOperationException();
-    }
-    
-    // SortedSet implementation
-    public Comparator comparator() {
-      return PropertyGroupSchedule.this.comparator();
-    }
-
-    public Object last() {
-      return PropertyGroupSchedule.this.last();
-    }
-
-    public int size() {
-      return PropertyGroupSchedule.this.size();
-    }
-  
-    public boolean isEmpty() {
-      return PropertyGroupSchedule.this.isEmpty();
-    }
-
-    public int indexOf(Object elem) {
-      return PropertyGroupSchedule.this.indexOf(elem);
-    }
-
-    public int lastIndexOf(Object elem) {
-      return PropertyGroupSchedule.this.lastIndexOf(elem);
-    }
-
-    public Object[] toArray() {
-      return PropertyGroupSchedule.this.toArray();
-    }
-
-    public Object[] toArray(Object a[]) {
-      return PropertyGroupSchedule.this.toArray(a);
-    }
-
-
-    public Object get(int index) {
-      return PropertyGroupSchedule.this.get(index);
-    }
-
-
-    public PropertyDescriptor[] getPropertyDescriptors() {
-      return properties;
-    }
-
-    public Class getIntrospectionClass() {
-      return PropertyGroupSchedule.class;
-    }
-
-  }
 
 }
 
