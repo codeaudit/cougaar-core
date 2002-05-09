3b377fe3cd17e117ecff38320adca7eab57a7799
ahelsing <ahelsing>
2002-05-09 20:42:50 +0000

    Add NAI javadoc

--- core/src/org/cougaar/core/service/MessageProtectionService.java
+++ core/src/org/cougaar/core/service/MessageProtectionService.java
@@ -30,26 +30,109 @@ import org.cougaar.core.mts.ProtectedInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/** Cryptographic Service used to cryptographically protect incoming
+ * and outgoing messages.
+ * This service should be called by the transport service for
+ * all Cougaar messages.
+ */
 public interface MessageProtectionService extends Service
 {
-    public byte[] protectHeader(byte[] rawData, 
-				MessageAddress source,
-				MessageAddress destination);
 
-    public byte[] unprotectHeader(byte[] rawData, 
-				  MessageAddress source,
-				  MessageAddress destination);
+  /**
+   * Sign and/or encrypt the header of an outgoing message.
+   *
+   * When a message is sent out:
+   * 1) The aspect calls protectHeader().
+   * 2) The data protection service encrypts/signs the header.
+   *    It uses the information provided in the source and destination
+   *    to decide how to encrypt and/or sign.
+   * 3) The encrypted header is returned.
+   * 4) The aspect calls getOuputStream.
+   *    - The source and destination should be the same as what was found
+   *      in the call to protectHeader().
+   * 5) The service returns an output stream where the MTS will serialize
+   *    the clear-text message.
+   * 6) The service encrypts the message and write the encrypte/signed
+   *    message to the output stream.
+   * 7) The encrypted message is actually sent over the network.
+   *
+   * @param rawData     The unencrypted header
+   * @param source      The source of the message
+   * @param destination The destination of the message
+   * @return the protected header (sign and/or encrypted)
+   */
+  byte[] protectHeader(byte[] rawData, 
+		       MessageAddress source,
+		       MessageAddress destination);
 
-    public ProtectedOutputStream getOutputStream(OutputStream os,
-						 MessageAddress src,
-						 MessageAddress dst,
-						 MessageAttributes attrs);
+  /**
+   * Verify the signed and/or encrypted header of an incoming message.
+   *
+   * @param rawData     The signed and/or encrypted header
+   * @param source      The source of the message
+   * @param destination The destination of the message
+   * @return the header in the clear
+   */
+  byte[] unprotectHeader(byte[] rawData, 
+			 MessageAddress source,
+			 MessageAddress destination);
 
-    public ProtectedInputStream getInputStream(InputStream is,
-					       MessageAddress src,
-					       MessageAddress dst,
-					       MessageAttributes attrs);
+  /** 
+   * Gets a stream to encrypt and/or sign outgoing messages
+   *
+   * This method is called once for each outgoing message.
+   * The implementation of this service must construct a
+   * ProtectedOutputStream, which is a special kind of FilterOutputStream.
+   * The service client (MTS) serializes a Message to this 
+   * ProtectedOutputStream. The implementation of the service will in turn
+   * write data to the 'os' stream it was given at creation time.
+   * When the Message has been completely serialized and written 
+   * to the ProtectedOutputStream, the service client calls the finish()
+   * method of the ProtectedOutputStream.
+   *
+   * The first byte of the ProtectedOutputStream should be the first byte
+   * of the (serialized) message content.
+   *
+   * Since messages may be resent, the method may be called multiple times
+   * for the same message, but this is in a different context.
+   *
+   * @param os The output stream containing encrypted and/or signed data
+   * @param source      The source of the outgoing message
+   * @param destination The destination of the outgoing message
+   * @param attrs       The attributes of the outgoing message
+   * @return A filter output stream
+   */
+  ProtectedOutputStream getOutputStream(OutputStream os,
+					MessageAddress src,
+					MessageAddress dst,
+					MessageAttributes attrs);
 
-
-
+  /** 
+   * Gets a stream to verify incoming messages
+   *
+   * This method is called once for each incoming message.
+   * The implementation of this service must construct a
+   * ProtectedInputStream, which is a special kind of FilterInputStream.
+   * The service reads an encrypted message from the ProtectedInputStream.
+   * The service client (MTS) calls the finishInput() method when all the
+   * message has been read.
+   * The service client verifies the message. The service client reads
+   * the clear-text message from the 'is' input stream.
+   *
+   * The first byte of the ProtectedInputStream should be the first byte
+   * of the (serialized) message content.
+   *
+   * Since messages may be resent, the method may be called multiple times
+   * for the same message, but this is in a different context.
+   *
+   * @param os The input stream containing the verified clear-text message
+   * @param source      The source of the incoming message
+   * @param destination The destination of the incoming message
+   * @param attrs       The attributes of the incoming message
+   * @return A filter intput stream
+   */
+  ProtectedInputStream getInputStream(InputStream is,
+				      MessageAddress src,
+				      MessageAddress dst,
+				      MessageAttributes attrs);
 }
