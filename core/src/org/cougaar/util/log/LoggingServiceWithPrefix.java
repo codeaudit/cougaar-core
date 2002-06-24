7873d7de1bc5ddfed17369a24d53606bac3ac7d2
tomlinso <tomlinso>
2002-06-24 16:48:22 +0000

    * org/cougaar/util/log/LoggingServiceWithPrefix.java: Bug #1533 -- Identification of malfunctioning agents is hard. Implementation of LoggingService that prefixes a string to every log message. The current implementation wraps a LoggingService with a version that adds a prefix.

---
+++ core/src/org/cougaar/util/log/LoggingServiceWithPrefix.java
@@ -0,0 +1,29 @@
+/*
+ * <copyright>
+ * Copyright 1997-2001 Defense Advanced Research Projects
+ * Agency (DARPA) and ALPINE (a BBN Technologies (BBN) and
+ * Raytheon Systems Company (RSC) Consortium).
+ * This software to be used only in accordance with the
+ * COUGAAR licence agreement.
+ * </copyright>
+ */
+
+package org.cougaar.util.log;
+
+import org.cougaar.util.log.LoggerAdapter;
+import org.cougaar.core.service.LoggingService;
+
+public class LoggingServiceWithPrefix extends LoggerAdapter implements LoggingService {
+  public static LoggingService add(LoggingService ls, String prefix) {
+    return new LoggingServiceWithPrefix(ls, prefix);
+  }
+
+  private String prefix;
+  private Logger logger;
+  public LoggingServiceWithPrefix(Logger logger, String prefix) {
+    this.logger = logger;
+    this.prefix = prefix;
+  }
+  public boolean isEnabledFor(int level) { return logger.isEnabledFor(level); }
+  public void log(int level, String message, Throwable t) { logger.log(level, prefix + message, t); }
+}  
