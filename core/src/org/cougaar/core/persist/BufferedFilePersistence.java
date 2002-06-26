5f8e68c0b5c6b1af7b066edb9ff9dfc8c6fd097f
tomlinso <tomlinso>
2002-06-26 12:42:23 +0000

    * BufferedFilePersistence.java: Enh #1547 -- Persists take long time, blocking agent progress Persistence plugin that soaks up persistence output into ram buffers and spools out the data to a (presumably slow) file system. This allows the writing of a persistence snapshot to complete more quickly and let the actual data writes proceed in the background.
    * BufferedFileSystem.java:
    Enh #1547 -- Persists take long time, blocking agent progress
    New file provides support for BufferedFilePersistence plugin.
    Opens buffered file output streams using a separate thread to
    spool the data to the file long after the stream has been written
    and closed. Uses up to 10 MBytes of buffering. Provides hooks for
    synchronizing renames and reads with the buffered output.

---
+++ core/src/org/cougaar/core/persist/BufferedFilePersistence.java
@@ -0,0 +1,74 @@
+
+/*
+ * <copyright>
+ *  Copyright 1997-2001 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+package org.cougaar.core.persist;
+
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+/**
+ * This persistence plugin saves and restores plan objects in
+ * files. There is one optional parameter naming the persistence
+ * root directory. If the parameter is omitted, the persistence root
+ * is specified by system properties.
+ * @property org.cougaar.install.path Used by FilePersistence as the
+ * parent directory for persistence snapshots when there is no
+ * directory specified in configuration parameters and
+ * org.cougaar.core.persistence.path is a relative pathname. This
+ * property is not used if the plugin is configured with a specific
+ * parameter specifying the location of the persistence root.
+ * @property org.cougaar.core.persistence.path Specifies the directory
+ * in which persistence snapshots should be saved. If this is a
+ * relative path, it the base will be the value or
+ * org.cougaar.install.path. This property is not used if the plugin
+ * is configured with a specific parameter specifying the location of
+ * the persistence root.
+ **/
+public class BufferedFilePersistence
+  extends FilePersistenceBase
+  implements PersistencePlugin
+{
+  BufferedFileSystem bfs;
+
+  public void init(PersistencePluginSupport pps, String name, String[] params)
+    throws PersistenceException
+  {
+    super.init(pps, name, params);
+    bfs = new BufferedFileSystem(pps.getLoggingService());
+  }
+
+  protected OutputStream openFileOutputStream(File file) throws FileNotFoundException {
+    return bfs.openOutputStream(file);
+  }
+
+  protected InputStream openFileInputStream(File file) throws FileNotFoundException {
+    return bfs.openInputStream(file);
+  }
+
+  protected boolean rename(File from, File to) {
+    return bfs.rename(from, to);
+  }
+}
---
+++ core/src/org/cougaar/core/persist/BufferedFileSystem.java
@@ -0,0 +1,267 @@
+
+/*
+ * <copyright>
+ *  Copyright 1997-2001 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+package org.cougaar.core.persist;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.cougaar.core.service.LoggingService;
+import org.cougaar.util.CircularQueue;
+
+/**
+ * A package to support buffered and otherwise queued access to the
+ * file system. Primarily, this serializes access to writing and
+ * renaming and reading files. All file write and rename actions
+ * return immediately without waiting for the actual filesystem
+ * operations to complete. File reads invariably block until prior
+ * writes and renames have concluded.
+ **/
+public class BufferedFileSystem implements Runnable {
+  private static final int BUFSIZE=100000;
+  private static final int MAXBUFFERS=100; // 10 Mbytes max
+  private static final int MAXKEPTBUFFERS=20; // 2 Mbytes max
+
+  private static List buffers = new ArrayList();
+  private static int totalBuffers = 0;
+
+  /**
+   * Get a buffer for writing. This is a bit complicated to avoid
+   * excessive allocation activity and excessive consumption of memory
+   * for this purpose. Up to MAXKEPTBUFFERS are allocated and reused
+   * freely. If demand exceeds MAXKEPTBUFFERS, additional buffers are
+   * allocated up to MAXBUFFERS. Demand in excess of MAXBUFFERS blocks.
+   **/
+  private static byte[] getBuffer() {
+    synchronized (buffers) {
+      while (totalBuffers >= MAXBUFFERS) {
+        try {
+          buffers.wait();
+        } catch (InterruptedException ie) {
+        }
+      }
+      int len = buffers.size();
+      if (len > 0) {
+        return (byte[]) buffers.remove(len - 1);
+      }
+      totalBuffers++;
+      return new byte[BUFSIZE];
+    }
+  }
+
+  private static void releaseBuffer(byte[] buf) {
+    synchronized (buffers) {
+      if (buffers.size() < MAXKEPTBUFFERS) {
+        buffers.add(buf);
+      } else {
+        totalBuffers--;
+      }
+      buffers.notifyAll();
+    }
+  }
+
+  private LoggingService logger;
+
+  private Thread thread;
+
+  private boolean active;
+
+  private CircularQueue queue = new CircularQueue();
+  private boolean executingTask = false;
+
+  /**
+   * Wrap a FileOutputStream to provide safe close semantics.
+   * Explicitly sync the file descriptor on close() to insure the file
+   * has been completely written to the disk.
+   **/
+  private class BufferedFileOutputStream extends OutputStream {
+    // The real OutputStream
+    private FileOutputStream fileOutputStream;
+    private byte[] buffer;
+    private int nbytes;
+    public BufferedFileOutputStream(FileOutputStream stream) {
+      fileOutputStream = stream;
+      newBuffer();
+    }
+
+    private void newBuffer() {
+      buffer = getBuffer();
+      nbytes = 0;
+    }
+
+    private void switchBuffer(final int nbytes) {
+      enqueueTask(new Runnable() {
+          byte[] buf = buffer;
+          public void run() {
+            try {
+              fileOutputStream.write(buf, 0, nbytes);
+            } catch (IOException ioe) {
+              throw new BufferedFileException(ioe);
+            }
+          }
+          public String toString() {
+            return "Write " + nbytes;
+          }
+        });
+      newBuffer();
+    }
+    
+    public void write(int b) throws IOException {
+      buffer[nbytes++] = (byte) b;
+      if (nbytes == BUFSIZE) switchBuffer(nbytes);
+    }
+
+    public void write(byte[] b, int offset, int nb) throws IOException {
+      while (nb > 0) {
+        int tnb = Math.min(nb, BUFSIZE - nbytes);
+        System.arraycopy(b, offset, buffer, nbytes, tnb);
+        nbytes += tnb;
+        if (nbytes == BUFSIZE) switchBuffer(nbytes);
+        offset += tnb;
+        nb -= tnb;
+      }
+    }
+
+    public void write(byte[] b) throws IOException {
+      write(b, 0, b.length);
+    }
+
+    public void flush() throws IOException {
+      if (nbytes > 0) switchBuffer(nbytes);
+    }
+
+    public void close() throws IOException {
+      flush();
+      enqueueTask(new Runnable() {
+          public void run() {
+            try {
+              fileOutputStream.flush();
+              fileOutputStream.getFD().sync();
+              fileOutputStream.close();
+            } catch (IOException ioe) {
+              throw new BufferedFileException(ioe);
+            }
+          }
+          public String toString() {
+            return "close";
+          }
+        });
+      if (logger.isInfoEnabled()) logger.info("Buffered closed");
+    }
+  }
+
+  private static class BufferedFileException extends RuntimeException {
+    public BufferedFileException(IOException t) {
+      super("Buffered IOException", t);
+    }
+  }
+
+  public BufferedFileSystem(LoggingService ls) {
+    logger = ls;
+  }
+
+  private void enqueueTask(Runnable task) {
+    synchronized (queue) {
+      queue.add(task);
+      if (thread == null) {
+        active = true;
+        thread = new Thread(this, "BufferedFileSystem");
+        thread.start();
+      }
+    }
+  }
+
+  public void run() {
+    while (true) {
+      Runnable task;
+      synchronized (queue) {
+        while (queue.size() == 0) {
+          try {
+            queue.wait();
+          } catch (InterruptedException ie) {
+          }
+          if (!active) return;
+        }
+        task = (Runnable) queue.next();
+        executingTask = true;
+      }
+      if (logger.isInfoEnabled()) logger.info("Buffered task " + task);
+      try {
+        task.run();
+      } catch (Throwable bfe) {
+        logger.error(bfe.getMessage(), bfe.getCause());
+      }
+      synchronized (queue) {
+        executingTask = false;
+        queue.notifyAll();
+      }
+    }
+  }
+
+  public void waitForPrevious() {
+    synchronized (queue) {
+      while (queue.size() > 0 || executingTask) {
+        try {
+          queue.wait();
+        } catch (InterruptedException ie) {
+        }
+      }
+    }
+  }
+
+  public void stop() {
+    synchronized (queue) {
+      active = false;
+      queue.notify();
+      try {
+        thread.join();
+      } catch (InterruptedException ie) {
+      }
+    }
+  }
+
+  public OutputStream openOutputStream(File file) throws FileNotFoundException {
+    return new BufferedFileOutputStream(new FileOutputStream(file));
+  }
+
+  public InputStream openInputStream(File file) throws FileNotFoundException {
+    waitForPrevious();
+    return new FileInputStream(file);
+  }
+
+  public boolean rename(final File from, final File to) {
+    enqueueTask(new Runnable() {
+        public void run() {
+          from.renameTo(to);
+        }
+        public String toString() {
+          return "rename " + from + " to " + to;
+        }
+      });
+    return true;
+  }
+}
