23135ff1ee7c9eca66d1bb62892737453fd9c6b4
tomlinso <tomlinso>
2003-03-17 19:47:15 +0000

    * AgentContainer.java: Bug #1848 -- Need persistence of component hierarchy for mobile agents Add Collection methods so components other than agents can be added/removed using this interface.

--- core/src/org/cougaar/core/agent/AgentContainer.java
+++ core/src/org/cougaar/core/agent/AgentContainer.java
@@ -23,6 +23,7 @@ package org.cougaar.core.agent;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.List;
 import org.cougaar.core.component.ComponentDescription;
 import org.cougaar.core.component.StateTuple;
 import org.cougaar.core.mts.MessageAddress;
@@ -67,6 +68,8 @@ public interface AgentContainer {
    */
   Map getAgents();
 
+  List getComponents();
+
   /**
    * Add a new agent to the local node.
    *
@@ -76,10 +79,24 @@ public interface AgentContainer {
   void addAgent(MessageAddress agentId, StateTuple tuple);
 
   /**
+   * Add a component to this agent container. Only certain components
+   * are allowed using this method. In particular, agents cannot be
+   * added.
+   **/
+  boolean add(Object o);
+
+  /**
    * Remove an agent that's on the local node.
    * 
    * @throws RuntimeException if the agent is not on the
    *    local node, or it can't be removed.
    */
   void removeAgent(MessageAddress agentId);
+
+  /**
+   * Remove component from this agent container. Only components added
+   * with the add method can be removed this way. Agents cannot be
+   * removed with this method.
+   **/
+  boolean remove(Object o);
 }
