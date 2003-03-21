c3149c4912953f04d943e02f3f8b501ce3728e5d
tomlinso <tomlinso>
2003-03-21 18:52:48 +0000

    * freeze/FreezeSocietyPlugin.java: Changes to support debugging Deletion. Added current execution time display and a field for specifying the execution rate.

--- core/src/org/cougaar/core/plugin/freeze/FreezeSocietyPlugin.java
+++ core/src/org/cougaar/core/plugin/freeze/FreezeSocietyPlugin.java
@@ -25,6 +25,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Set;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import javax.servlet.Servlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -48,6 +50,8 @@ public class FreezeSocietyPlugin extends FreezeSourcePlugin {
   private WhitePagesService wps = null;
   protected String status = "Running";
   private DemoControlService demoControlService;
+  private double savedRate = 1.0;
+  private double newRate = Double.NaN;
 
   public void setDemoControlService(DemoControlService dcs) {
     demoControlService = dcs;
@@ -76,15 +80,21 @@ public class FreezeSocietyPlugin extends FreezeSourcePlugin {
     if (logger.isDebugEnabled()) logger.debug("unfrozen" + unfrozenAgents);
     if (unfrozenAgents.isEmpty()) {
       status = "Frozen";
-      demoControlService.setTimeRate(0.0);
+      savedRate = demoControlService.getExecutionRate();
+      setRate(0.0);
     } else {
       status = "Freezing " + unfrozenAgents;
     }
   }
 
+  private synchronized void setRate(double newRate) {
+    this.newRate = newRate;
+    demoControlService.setTimeRate(newRate);
+  }
+
   protected synchronized void thaw() {
     super.thaw();
-    demoControlService.setTimeRate(1.0);
+    setRate(savedRate);
   }
 
   protected Set getTargetNames() {
@@ -100,6 +110,13 @@ public class FreezeSocietyPlugin extends FreezeSourcePlugin {
     return names;
   }
 
+  private static final SimpleDateFormat timeFormat =
+    new SimpleDateFormat("yyyy/MM/dd HHmm zzz");
+
+  private static String formatTime(long time) {
+    return timeFormat.format(new Date(time));
+  }
+
   private class FreezeControlServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
       doPostOrGet(request, response, false);
@@ -111,36 +128,66 @@ public class FreezeSocietyPlugin extends FreezeSourcePlugin {
       throws IOException
     {
       String submit = request.getParameter("submit");
+      String rate = request.getParameter("rate");
       boolean freeze = "Freeze".equals(submit);
       boolean thaw = "Thaw".equals(submit);
+      boolean setRate = "Set Execution Rate".equals(submit) || submit==null;
+      double erate = demoControlService.getExecutionRate();
       String tstatus = status;
-      if (freeze || thaw) {
+      if (freeze) {
         blackboard.openTransaction();
         synchronized (FreezeSocietyPlugin.this) {
-          if (freeze) {
-            freeze();
-            status = "Freezing...";
-          } else if (thaw) {
-            thaw();
-            status = "Running";
-          }
+          freeze();
+          status = "Freezing...";
           tstatus = status;
         }
         blackboard.closeTransactionDontReset();
+      } else if (thaw) {
+        blackboard.openTransaction();
+        synchronized (FreezeSocietyPlugin.this) {
+          thaw();
+          status = "Running";
+          tstatus = status;
+        }
+        blackboard.closeTransactionDontReset();
+      } else if (setRate) {
+        try {
+          setRate(Double.parseDouble(rate));
+        } catch (Exception e) {
+        }
       }
       response.setContentType("text/html");
       if (tstatus.startsWith("Freezing")) {
         response.setHeader("Refresh", "1");
       }
+      if (Math.abs(erate - newRate) / (erate + newRate) > 0.0001) {
+        rate = String.valueOf(erate) + "->" + String.valueOf(newRate);
+        response.setHeader("Refresh", "1");
+      } else {
+        rate = String.valueOf(erate);
+      }
       PrintWriter out = response.getWriter();
-      out.println("<html>\n  <head>\n    <title>Society Freeze Control</title>\n  </head>");
-      out.println("  <body>\n    <h1>Society Freeze Control</h1>"
-                  + "    <form action=\"freezeControl\" method=\"post\">");
+      out.println("<html>");
+      out.println("  <head>");
+      out.println("    <title>Society Freeze Control</title>");
+      out.println("  </head>");
+      out.println("  <body>");
+      out.println("    <h1>Society Freeze Control</h1>");
+      out.println("    <form action=\"freezeControl\" method=\"post\">");
+      // The following kludge the browser bugs that include the first
+      // submit button if <enter> is typed in a text field.
+      out.println("     <input type=\"image\" border=\"0\" name=\"refresh\" width=1 height=1 src=\"transprent-1x1.gif\"><br>");
       out.println(tstatus + "<br>");
       out.println("<input type=\"submit\" name=\"submit\" value=\"Freeze\">");
-      out.println("<input type=\"submit\" name=\"submit\" value=\"Thaw\">");
+      out.println("<input type=\"submit\" name=\"submit\" value=\"Thaw\"><br>");
+      out.println("Execution Time: " + formatTime(currentTimeMillis()) + "<br>");
+      out.println("Execution Rate: ");
+      out.println("<input type=\"text\" name=\"rate\" value=\"" + rate + "\">");
+      out.println("<input type=\"submit\" name=\"submit\" value=\"Set Execution Rate\"><br>");
       out.println("<input type=\"submit\" name=\"submit\" value=\"Refresh\">");
-      out.println("    </form>\n  </body>\n</html>");
+      out.println("    </form>");
+      out.println("  </body>");
+      out.println("</html>");
     }
   }
 }
