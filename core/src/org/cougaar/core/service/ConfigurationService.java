88e2efad0d23dd860eb7e2c524d4dea95772b4c0
mthome <mthome>
2004-04-19 15:18:18 +0000

    Add ConfigurationService definition (no implementation yet) - partial cbug 2569

---
+++ core/src/org/cougaar/core/service/ConfigurationService.java
@@ -0,0 +1,80 @@
+/*
+ * <copyright>
+ *  
+ *  Copyright 2003-2004 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ * 
+ *  You can redistribute this software and/or modify it under the
+ *  terms of the Cougaar Open Source License as published on the
+ *  Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * </copyright>
+ */
+
+package org.cougaar.core.service;
+
+import org.cougaar.core.component.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+import org.w3c.dom.Document;
+
+/**
+ * ConfigurationService is a service interface front for org.cougaar.util.ConfigFinder.
+ **/
+
+public interface ConfigurationService extends Service {
+  /** the (immutable) set of configuration URLs used by this Service **/
+  List getConfigurationPath();
+  
+  /**
+   * Locate an actual file in the config path. This will skip over
+   * elements of org.cougaar.config.path that are not file: urls.
+   **/
+  File locateFile(String filename);
+
+  /**
+   * Resolve a logical reference to a URL.
+   * @return null if unresolvable.
+   **/
+  URL resolveName(String logicalName) throws MalformedURLException;
+
+  /**
+   * Opens an InputStream to access the named file. The file is sought
+   * in all the places specified in configPath.
+   * @throws IOException if the resource cannot be found.
+   **/
+  InputStream open(String aURL) throws IOException;
+
+  /**
+   * Attempt to find the URL which would be opened by the open method.
+   * Note that this must actually attempt to open the various URLs
+   * under consideration, so this is <em>not</em> an inexpensive operation.
+   **/
+  URL find(String aURL) throws IOException;
+
+  /** Read and parse an XML file somewhere in the configpath **/
+  Document parseXMLConfigFile(String xmlfile) throws IOException;
+}
+
