c61d6850670322082b928d899643d5df8c18d425
twright <twright>
2007-09-12 15:01:10 +0000

    Renamed class from "BaseUniqueObject" to "UniqueObjectBase".

--- core/src/org/cougaar/core/util/BaseUniqueObject.java
+++ core/src/org/cougaar/core/util/BaseUniqueObject.java
@@ -1,28 +1,8 @@
 package org.cougaar.core.util;
 
-/**
- * Minimal base class for UniqueObjects that use the UID for equality
- */
-public class BaseUniqueObject implements UniqueObject {
-    private final UID uid;
-
-    public BaseUniqueObject(UID uid) {
-        this.uid = uid;
-    }
-
-    public UID getUID() {
-        return uid;
-    }
-
-    public boolean equals(Object o) {
-        return (o == this) || ((o instanceof BaseUniqueObject) && uid.equals(((BaseUniqueObject) o).uid));
-    }
-
-    public int hashCode() {
-        return uid.hashCode();
-    }
-
-    public void setUID(UID uid) {
-        throw new IllegalStateException("UID cannot be changed");
-    }
+/** @deprecated use UniqueObjectBase */
+public class BaseUniqueObject extends UniqueObjectBase {
+  public BaseUniqueObject(UID uid) {
+    super(uid);
+  }
 }
