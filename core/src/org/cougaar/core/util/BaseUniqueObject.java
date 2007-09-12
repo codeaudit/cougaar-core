d6606ae3d04b2e4a4ee46dd98b3bb4179de91137
twright <twright>
2007-09-12 20:20:53 +0000

    Renamed to UniqueObjectBase.

--- core/src/org/cougaar/core/util/BaseUniqueObject.java
+++
@@ -1,8 +0,0 @@
-package org.cougaar.core.util;
-
-/** @deprecated use UniqueObjectBase */
-public class BaseUniqueObject extends UniqueObjectBase {
-  public BaseUniqueObject(UID uid) {
-    super(uid);
-  }
-}
