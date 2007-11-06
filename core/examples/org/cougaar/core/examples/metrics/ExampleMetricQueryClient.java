5534ecd6dde19f01c1fc4397086f9ef5d9c7473f
jzinky <jzinky>
2007-11-06 17:34:02 +0000

    Made an understandable example of a java-based metric query client

--- core/examples/org/cougaar/core/examples/metrics/ExampleMetricQueryClient.java
+++ core/examples/org/cougaar/core/examples/metrics/ExampleMetricQueryClient.java
@@ -23,60 +23,69 @@
  *  
  * </copyright>
  */
-/*
- * Stand alone java client that opens up a URL connection, reads an object stream and 
- * outputs the result (will be a java ArrayList of 'Path|Metric's ) 
- * Usage: java -cp . ExampleMetricQueryClient "http://localhost:8800/\$3-69-ARBN/metrics/query?format=java&paths=Agent(3-69-ARBN):SpokeTime|IpFlow(blatz,stout):CapacityMax"
- * Must specify 'format=java' as the default is a string of xml printed out to browser
- */
+
 package org.cougaar.core.examples.metrics;
 
-import java.net.URL;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.io.ByteArrayInputStream;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.net.URL;
 import java.util.Map;
+import java.util.Set;
 
-// This example requires quoSumo.jar, qos.jar and core.jar
-public class ExampleMetricQueryClient
-{
-  // One arg here - the query string "http://host..." see usage above
-  public static void main( String args[] ) throws IOException
-  {
-    String url = null;
-    try {
-      url=args[0];
-      URL servlet_url = new URL(url);
-      
-      // open up input stream and read object
-      try {
-	// open url connection from string url
-	InputStream in = servlet_url.openStream();
-	ObjectInputStream ois = new ObjectInputStream(in);
-	
-	// read in java object - this a java client only
-	HashMap propertylist = (HashMap)ois.readObject();
-	ois.close();
+import org.cougaar.core.qos.metrics.Metric;
 
-	if (propertylist == null) {
-	    System.out.println("Null Property List returned");
-	    return;
-	}
-	// can do anything with it here, we just print it out for now
-	Iterator itr = propertylist.entrySet().iterator();
-	while (itr.hasNext()) {
-	    Map.Entry entry = (Map.Entry) itr.next();
-	    System.out.println(entry.getKey() +"->"+ entry.getValue());
-	}
-      } catch (Exception e) {
-	System.out.println("Error reading input stream for url " + url + " Make sure the stream is open. " + e);
-	e.printStackTrace();
-      }
-    } catch(Exception e){
-      System.out.println("Unable to acquire URL, Exception: " + e);
+/**
+ * Standalone java client that opens a URL connection, reads an object stream and 
+ * prints the result.
+ * <p> The URL should be a metric query directed at the MetricsQueryServlet for
+ * some agent.  The object in the reply stream will of type 
+ * <code>Map&lt;String, Metric&gt;</code>, where
+ * the keys are the paths in the query portion of the URL:
+ * <pre>
+ * Usage: java -cp . ExampleMetricQueryClient "http://localhost:8800/\$3-69-ARBN/metrics/query?format=java&paths=Agent(3-69-ARBN):SpokeTime|IpFlow(blatz,stout):CapacityMax"
+ *</pre>
+ *<p> You must specify 'format=java' to get a Map back, since the default behavior is to print
+ * a string of xml to a web browser.
+ * <p>
+ * This example requires qos.jar and qrs.jar 
+ */
+public class ExampleMetricQueryClient {
+    private static final String DefaultQuery = 
+       " http://localhost:8800/$/metrics/query?format=java&paths=$(localhost):EffectiveMJips|$(localhost):LoadAverage";
+    // One arg here - the query string "http://host..." see usage above
+    public static void main(String args[]) {
+        String url = null;
+        try {
+            url = args.length > 0 ? args[0] : DefaultQuery;
+            URL servlet_url = new URL(url);
+
+            // open up input stream and read object
+            try {
+                // open url connection from string url
+                InputStream in = servlet_url.openStream();
+                ObjectInputStream ois = new ObjectInputStream(in);
+
+                // read in java object - this a java client only
+                @SuppressWarnings("unchecked")
+                Map<String, Metric> propertylist = (Map<String, Metric>) ois.readObject();
+                ois.close();
+
+                if (propertylist == null) {
+                    System.out.println("Null Property List returned");
+                    return;
+                }
+                Set<Map.Entry<String, Metric>> entries = propertylist.entrySet();
+                // can do anything with it here, we just print it out for now
+                for (Map.Entry<String, Metric> entry : entries) {
+                    System.out.println(entry.getKey() + "->" + entry.getValue());
+                }
+            } catch (Exception e) {
+                System.out.println("Error reading input stream for url " + url
+                        + " Make sure the stream is open. " + e);
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            System.out.println("Unable to acquire URL, Exception: " + e);
+        }
     }
-  }  
 }
