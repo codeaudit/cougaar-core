8412847e9c9418b8d1975ff2f98d0c7bce431e2b
R Shapiro <rshapiro@bbn.com>
2012-05-25 15:24:19 -0400

    Refactor to support Query annotation

--- core/src/org/cougaar/core/plugin/AnnotatedSubscriptionsPlugin.java
+++ core/src/org/cougaar/core/plugin/AnnotatedSubscriptionsPlugin.java
@@ -6,6 +6,7 @@
 
 package org.cougaar.core.plugin;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -20,221 +21,275 @@ import org.cougaar.util.annotations.Cougaar;
 import org.cougaar.util.annotations.Subscribe;
 
 /**
- * This class provides support for plugins that wish
- * to use annotations to create and manage blackboard
- * IncrementalSubscriptions.
+ * This class provides support for plugins that wish to use annotations to
+ * create and manage blackboard IncrementalSubscriptions.
  */
-public abstract class AnnotatedSubscriptionsPlugin extends ParameterizedPlugin {
-    private final Map<String, IncrementalSubscription> subscriptions = 
-        new HashMap<String, IncrementalSubscription>();
-    private final List<Invoker> invokers = new ArrayList<Invoker>();
-    
-    @Override
+public abstract class AnnotatedSubscriptionsPlugin
+      extends ParameterizedPlugin {
+   private final Map<String, IncrementalSubscription> subscriptions = new HashMap<String, IncrementalSubscription>();
+   private final List<SubscriptionInvoker> subscriptionInvokers = new ArrayList<SubscriptionInvoker>();
+   private final List<QueryRunner> queryRunners = new ArrayList<QueryRunner>();
+
+   @Override
    protected void execute() {
-        for (Invoker invoker : invokers) {
-            invoker.execute();
-        }
-     }
-    
-    @Override
+      for (SubscriptionInvoker invoker : subscriptionInvokers) {
+         invoker.execute();
+      }
+   }
+
+   public void runQueries() {
+      for (QueryRunner runner : queryRunners) {
+         runner.execute();
+      }
+   }
+   
+   @Override
    protected void setupSubscriptions() {
-        Class<? extends AnnotatedSubscriptionsPlugin> klass = getClass();
-        // Method[] methods = klass.getMethods();
-        Collection<Method> methods = Cougaar.getAnnotatedMethods(klass, Cougaar.Execute.class);
-        for (Method method : methods) {
-            Cougaar.Execute annotation = method.getAnnotation(Cougaar.Execute.class);
-            String id;
-            String when = annotation.when();
-            if (!Cougaar.NO_VALUE.equals(when)){
-                id = when;
-            } else {
-                // Use the type of the first arg as an implicit 'isa'
-                Class<?>[] parameterTypes = method.getParameterTypes();
-                if (parameterTypes.length < 1 || parameterTypes.length > 2) {
-                    String message = "@Execute method" + method.getName() 
-                    + " of class " +getClass().getName()+
-                    " has the wrong number of arguments (should be 1 or 2)";
-                    log.error(message);
-                    throw new IllegalArgumentException(message);
-                }
-                if (parameterTypes.length == 2) {
-                    // ensure 2nd arg is IncrementalSubscription
-                    Class<?> cls = parameterTypes[1];
-                    if (!IncrementalSubscription.class.isAssignableFrom(cls)) {
-                        String message = "@Execute method" + method.getName()
-                        + " of class " +getClass().getName()+
-                        " has an invalid second argument (should be IncrementalSubscription)";
-                        log.error(message);
-                        throw new IllegalArgumentException(message);
-                    }
-                }
-                id = parameterTypes[0].getName();
-            }
-            IncrementalSubscription subscription = subscriptions.get(id);
-            if (subscription != null) {
-                Invoker invoker = new Invoker(method, annotation, subscription);
-                invokers.add(invoker);
-            } else {
-                Invoker invoker = new Invoker(method, annotation);
-                subscriptions.put(id, invoker.sub);
-                invokers.add(invoker);
-            }
-        }
-    }
-    
-    public IncrementalSubscription getSubscription(String id) {
-        return subscriptions.get(id);
-    }
-    
-    
+      Class<? extends AnnotatedSubscriptionsPlugin> klass = getClass();
+      // Method[] methods = klass.getMethods();
+      createSubsscriptionInvokers(klass);
+      createQueryRunners(klass);
+   }
 
-    /**
-     * Keeps the state of one IncrementalSubscription created via annotations.
-     */
-    private class Invoker {
-        /**
-         * The subscription itself.
-         */
-        private final IncrementalSubscription sub;
-        
-        /**
-         * The annotated method which will be invoked on the plugin, passing in each element
-         * of an IncrementalSubscription collection in turn as an argument.
-         */
-        private final Method method;
-        
-        /**
-         * The set of operations, as given in the annotation, which determines
-         * which ncrementalSubscription collection are relevant.
-         */
-        private final Subscribe.ModType[] ops;
-        
-        public Invoker(Method method, Cougaar.Execute annotation, IncrementalSubscription sub) {
-            this.method = method;
-            this.ops = annotation.on();
-            this.sub = sub;
-        }
-        
-        public Invoker(Method method, Cougaar.Execute annotation) {
-            this.method = method;
-            this.ops = annotation.on();
-            this.sub = createIncrementalSubscription(annotation);
+   private void createQueryRunners(Class<? extends AnnotatedSubscriptionsPlugin> klass) {
+      Collection<Method> methods = Cougaar.getAnnotatedMethods(klass, Cougaar.Query.class);
+      for (Method method : methods) {
+         // Use the type of the first arg as an implicit 'isa'
+         Class<?>[] parameterTypes = method.getParameterTypes();
+         if (parameterTypes.length != 1) {
+            String message =
+                  "@Query method" + method.getName() + " of class " + getClass().getName()
+                        + " has the wrong number of arguments (should be 1)";
+            log.error(message);
+            throw new IllegalArgumentException(message);
+         }
+         Cougaar.Query annotation = method.getAnnotation(Cougaar.Query.class);
+         QueryRunner runner = new QueryRunner(method, annotation);
+         queryRunners.add(runner);
+      }
+   }
+
+   private void createSubsscriptionInvokers(Class<? extends AnnotatedSubscriptionsPlugin> klass) {
+      Collection<Method> methods = Cougaar.getAnnotatedMethods(klass, Cougaar.Execute.class);
+      for (Method method : methods) {
+         Cougaar.Execute annotation = method.getAnnotation(Cougaar.Execute.class);
+         String id;
+         String when = annotation.when();
+         if (!Cougaar.NO_VALUE.equals(when)) {
+            id = when;
+         } else {
+            // Use the type of the first arg as an implicit 'isa'
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            if (parameterTypes.length < 1 || parameterTypes.length > 2) {
+               String message =
+                     "@Execute method" + method.getName() + " of class " + getClass().getName()
+                           + " has the wrong number of arguments (should be 1 or 2)";
+               log.error(message);
+               throw new IllegalArgumentException(message);
+            }
+            if (parameterTypes.length == 2) {
+               // ensure 2nd arg is IncrementalSubscription
+               Class<?> cls = parameterTypes[1];
+               if (!IncrementalSubscription.class.isAssignableFrom(cls)) {
+                  String message =
+                        "@Execute method" + method.getName() + " of class " + getClass().getName()
+                              + " has an invalid second argument (should be IncrementalSubscription)";
+                  log.error(message);
+                  throw new IllegalArgumentException(message);
+               }
+            }
+            id = parameterTypes[0].getName();
+         }
+         IncrementalSubscription subscription = subscriptions.get(id);
+         if (subscription != null) {
+            SubscriptionInvoker invoker = new SubscriptionInvoker(method, annotation, subscription);
+            subscriptionInvokers.add(invoker);
+         } else {
+            SubscriptionInvoker invoker = new SubscriptionInvoker(method, annotation);
+            subscriptions.put(id, invoker.sub);
+            subscriptionInvokers.add(invoker);
+         }
+      }
+   }
+
+   public IncrementalSubscription getSubscription(String id) {
+      return subscriptions.get(id);
+   }
+
+   @SuppressWarnings("deprecation")
+   private boolean isTesterMethod(Method method, String name, Class<?> argClass) {
+      if (method.isAnnotationPresent(Cougaar.Predicate.class)) {
+         Cougaar.Predicate pred = method.getAnnotation(Cougaar.Predicate.class);
+         if (!name.equals(pred.when())) {
+            return false;
+         }
+      } else if (!name.equals(method.getName())) {
+         return false;
+      }
+      Class<?>[] paramTypes = method.getParameterTypes();
+      if (paramTypes.length != 1) {
+         return false;
+      }
+      if (!argClass.isAssignableFrom(paramTypes[0])) {
+         return false;
+      }
+      Class<?> returnType = method.getReturnType();
+      return returnType == Boolean.class || returnType == boolean.class;
+   }
+
+   private UnaryPredicate createPredicate(Method method, String testerMethodName) {
+      Class<?> pluginClass = AnnotatedSubscriptionsPlugin.this.getClass();
+      Method[] methods = pluginClass.getMethods();
+      Class<?> argClass = method.getParameterTypes()[0];
+      Method testerMethod = null;
+      for (Method candidate : methods) {
+         if (isTesterMethod(candidate, testerMethodName, argClass)) {
+            testerMethod = candidate;
+            break;
+         }
+      }
+      if (testerMethod == null) {
+         String message =
+               "@Execute method" + method.getName() + " of class " + getClass().getName()
+                     + " references unknown predicate method " + testerMethodName + " (" + argClass + ")";
+         log.error(message);
+         throw new IllegalArgumentException(message);
+      }
+      final Method finalTesterMethod = testerMethod;
+      final Class<?> testerArgClass = testerMethod.getParameterTypes()[0];
+      UnaryPredicate predicate = new UnaryPredicate() {
+         private static final long serialVersionUID = 1L;
+   
+         public boolean execute(Object o) {
+            if (!testerArgClass.isAssignableFrom(o.getClass())) {
+               return false;
+            }
+            try {
+               return (Boolean) finalTesterMethod.invoke(AnnotatedSubscriptionsPlugin.this, o);
+            } catch (Exception e) {
+               log.error("Test failed", e);
+               return false;
+            }
+         }
+   
+      };
+      return predicate;
+   }
+
+   private class QueryRunner {
+      private final Method method;
+      private UnaryPredicate predicate;
+
+      QueryRunner(Method method, Cougaar.Query annotation) {
+         this.method = method;
+         predicate = createPredicate(method, annotation.where());
+      }
+      
+      void execute() {
+         Collection matches = blackboard.query(predicate);
+         for (Object match : matches) {
+            try {
+               method.invoke(AnnotatedSubscriptionsPlugin.this, match);
+            } catch (IllegalArgumentException e) {
+               log.error("Failed to invoke annotated method", e);
+            } catch (IllegalAccessException e) {
+               log.error("Failed to invoke annotated method", e);
+            } catch (InvocationTargetException e) {
+               log.error("Failed to invoke annotated method", e);
+            }
+         }
+      }
+   }
+
+   /**
+    * Keeps the state of one IncrementalSubscription created via annotations.
+    */
+   private class SubscriptionInvoker {
+      /**
+       * The subscription itself.
+       */
+      private final IncrementalSubscription sub;
+
+      /**
+       * The annotated method which will be invoked on the plugin, passing in
+       * each element of an IncrementalSubscription collection in turn as an
+       * argument.
+       */
+      private final Method method;
+
+      /**
+       * The set of operations, as given in the annotation, which determines
+       * which ncrementalSubscription collection are relevant.
+       */
+      private final Subscribe.ModType[] ops;
+
+      public SubscriptionInvoker(Method method, Cougaar.Execute annotation, IncrementalSubscription sub) {
+         this.method = method;
+         this.ops = annotation.on();
+         this.sub = sub;
+      }
+
+      public SubscriptionInvoker(Method method, Cougaar.Execute annotation) {
+         this.method = method;
+         this.ops = annotation.on();
+         this.sub = createIncrementalSubscription(annotation);
+
+      }
+
+      private IncrementalSubscription createIncrementalSubscription(Cougaar.Execute annotation) {
+         String when = annotation.when();
+         UnaryPredicate predicate;
+         if (Cougaar.NO_VALUE.equals(when)) {
+            // Implicit instanceof if no 'when'
+            Class<?> isa = method.getParameterTypes()[0];
+            predicate = new IsInstanceOf(isa);
+         } else {
             
-        }
+            predicate = createPredicate(method, when);
+         }
+         return (IncrementalSubscription) blackboard.subscribe(predicate);
+      }
 
-        @SuppressWarnings("deprecation")
-        private boolean isTesterMethod(Method method, String name, Class<?> argClass) {
-            if (method.isAnnotationPresent(Cougaar.Predicate.class)) {
-                Cougaar.Predicate pred = method.getAnnotation(Cougaar.Predicate.class);
-                if (!name.equals(pred.when())) {
-                    return false;
-                }
-            } else if (!name.equals(method.getName())) {
-                return false;
-            }
-            Class<?>[] paramTypes = method.getParameterTypes();
-            if (paramTypes.length != 1) {
-                return false;
-            }
-            if (!argClass.isAssignableFrom(paramTypes[0])) {
-                return false;
-            }
-            Class<?> returnType = method.getReturnType();
-            return returnType == Boolean.class || returnType == boolean.class;
-        }
-        
-        private IncrementalSubscription createIncrementalSubscription(Cougaar.Execute annotation) {
-            String when = annotation.when();
-            UnaryPredicate predicate;
-            if (Cougaar.NO_VALUE.equals(when)) {
-                // Implicit instanceof if no 'when'
-                Class<?> isa = method.getParameterTypes()[0];
-                predicate = new IsInstanceOf(isa);
-            } else {
-                // Construct a UnaryPredicate from the 'when'
-                Class<?> pluginClass = AnnotatedSubscriptionsPlugin.this.getClass();
-                Method[] methods = pluginClass.getMethods();
-                Class<?> argClass = method.getParameterTypes()[0];
-                Method testerMethod = null;
-                for (Method method : methods) {
-                    if (isTesterMethod(method, when, argClass)) {
-                        testerMethod = method;
-                        break;
-                    }
-                }
-                if (testerMethod == null) {
-                    String message = "@Execute method" + method.getName()
-                    + " of class " +getClass().getName()
-                    + " references unknown predicate method " 
-                        + when+ " (" + argClass + ")";
-                    log.error(message);
-                    throw new IllegalArgumentException(message);
-                }
-                final Method tester = testerMethod;
-                final Class<?> testerArgClass = tester.getParameterTypes()[0];
-                predicate = new UnaryPredicate() {
-                    /**
-                   * 
-                   */
-                  private static final long serialVersionUID = 1L;
+      private Collection<?> getCollection(Subscribe.ModType op) {
+         switch (op) {
+            case ADD:
+               return sub.getAddedCollection();
 
-                  public boolean execute(Object o) {
-                        if (!testerArgClass.isAssignableFrom(o.getClass())) {
-                            return false;
-                        }
-                        try {
-                            return (Boolean) tester.invoke(AnnotatedSubscriptionsPlugin.this, o);
-                        } catch (Exception e) {
-                            log.error("Test failed", e);
-                            return false;
-                        }
-                    }
+            case CHANGE:
+               return sub.getChangedCollection();
 
-                };
-            }
-            return (IncrementalSubscription) blackboard.subscribe(predicate);
-        }
+            case REMOVE:
+               return sub.getRemovedCollection();
 
-        private Collection<?> getCollection(Subscribe.ModType op) {
-            switch (op) {
-                case ADD:
-                    return sub.getAddedCollection();
-                    
-                case CHANGE:
-                    return sub.getChangedCollection();
-                    
-                case REMOVE:
-                    return sub.getRemovedCollection();
-                    
-                default:
-                    return null;
+            default:
+               return null;
+         }
+      }
+
+      public void execute() {
+         if (sub == null || !sub.hasChanged()) {
+            // failed to make a proper subscription, or no changes
+            return;
+         }
+         boolean includeSubscription = method.getParameterTypes().length == 2;
+         for (Subscribe.ModType op : ops) {
+            Collection<?> objects = getCollection(op);
+            if (objects != null) {
+               for (Object object : objects) {
+                  try {
+                     AnnotatedSubscriptionsPlugin plugin = AnnotatedSubscriptionsPlugin.this;
+                     if (includeSubscription) {
+                        method.invoke(plugin, object, sub);
+                     } else {
+                        method.invoke(plugin, object);
+                     }
+                  } catch (Exception e) {
+                     log.error("Failed to invoke annotated method", e);
+                  }
+               }
             }
-        }
-        
-        public void execute() {
-            if (sub == null || !sub.hasChanged()) {
-                // failed to make a proper subscription, or no changes
-                return;
-            }
-            boolean includeSubscription = method.getParameterTypes().length == 2;
-            for (Subscribe.ModType op : ops) {
-                Collection<?> objects = getCollection(op);
-                if (objects != null) {
-                    for (Object object : objects) {
-                        try {
-                            AnnotatedSubscriptionsPlugin plugin = AnnotatedSubscriptionsPlugin.this;
-                            if (includeSubscription) {
-                                method.invoke(plugin, object, sub);
-                            } else {
-                                method.invoke(plugin, object);
-                            }
-                        } catch (Exception e) {
-                            log.error("Failed to invoke annotated method", e);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
+         }
+      }
+   }
+
 }
