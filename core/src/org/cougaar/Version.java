569158603b449dc03b82dd9c4630f79fc801adfe
David Waitzman <djw@bbn.com>
2012-06-06 08:03:47 -0400

    Generated Version.java from version.def; renamed version.def to version.def-obsolete; removed .gitignore that ignored Version.java

---
+++ core/src/org/cougaar/Version.java
@@ -0,0 +1,40 @@
+/*
+ * <copyright>
+ *  
+ *  Copyright 1997-2012 Raytheon BBN Technologies
+ *  under partial sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ * 
+ *  You can redistribute this software and/or modify it under the
+ *  terms of the Cougaar Open Source License as published on the
+ *  Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * </copyright>
+ */
+
+/* @generated Wed Jun 06 07:47:27 EDT 2012 from version.def - DO NOT HAND EDIT */
+
+package .;
+public final class Version {
+  public final static String version = "12.6";
+  public final static long buildTime = 1338983247287L;
+  public final static String repositoryTag = null;
+  public final static boolean repositoryModified = false;
+  public final static long repositoryTime = -1L;
+  public final static void main(String args[]) {
+    System.out.println("version="+version);
+    System.out.println("build time="+new java.util.Date(buildTime));
+  }
+}
